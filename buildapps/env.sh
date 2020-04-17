c948387c03bbe2fff5a0e8db5a5500a745799703
Aju Ajmal <dev@ajmalaju.com>
2020-04-18 05:02:44 +0530

    move data to firebase

--- buildapps/env.sh
+++ buildapps/env.sh
@@ -1,8 +1,14 @@
 set -eu
 
-echo "REACT_APP_apiKey = {$react_env}" >> mal
-cat mal
-echo "REACT_APP_apiKey = {$react_env}" >> mal
-echo "REACT_APP_apiKey = {$react_env}" >> mal
-echo "REACT_APP_apiKey = {$react_env}" >> mal
-cat mal
+cd github-cards
+
+echo "REACT_APP_apiKey={$fb_gc_apikey}" >> .env
+echo "REACT_APP_appId={$fb_gc_appId}" >> .env
+echo "REACT_APP_authDomain={$fb_gc_authDomain}" >> .env
+echo "REACT_APP_databaseURL={$fb_gc_databaseURL}" >> .env
+echo "REACT_APP_measurementId={$fb_gc_measurementId}" >> .env
+echo "REACT_APP_messagingSenderId={$fb_gc_messagingSenderId}" >> .env
+echo "REACT_APP_projectId={$fb_gc_projectId}" >> .env
+echo "REACT_APP_storageBucket={$fb_gc_storageBucket}" >> .env
+
+echo "Finish the env var set for github cards"
