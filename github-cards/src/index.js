2766d7ad5adce6b2ef87177557a9cb3e18a97c4e
Aju Ajmal <dev@ajmalaju.com>
2020-04-24 21:17:31 +0530

    update build

--- github-cards/src/index.js
+++ github-cards/src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
+import * as serviceWorker from './serviceWorker';
+
 
 //import AppF from './App/Using-Function';
 //import AppFP from './App/Using-Func-Dir-Props';
@@ -17,8 +19,7 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-////import * as serviceWorker from './serviceWorker';
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-//serviceWorker.unregister();
+serviceWorker.unregister();
