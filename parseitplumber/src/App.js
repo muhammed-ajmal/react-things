02a2411e9ac95c32339afcd7cb617627cc7b625a
Aju Ajmal <dev@ajmalaju.com>
2020-07-17 18:10:38 +0530

    parse any csv

--- parseitplumber/src/App.js
+++ parseitplumber/src/App.js
@@ -7,6 +7,60 @@ import axios from 'axios';
 
 const API_BASE = "https://rapi.ajmalaju.com"
 
+
+class Table extends React.Component {
+
+ constructor(props){
+ super(props);
+ this.getHeader = this.getHeader.bind(this);
+ this.getRowsData = this.getRowsData.bind(this);
+ this.getKeys = this.getKeys.bind(this);
+ }
+
+ getKeys = function(){
+  return Object.keys(this.props.data[0]);
+  }
+
+  getHeader = function(){
+   var keys = this.getKeys();
+   return keys.map((key, index)=>{
+   return <th key={key}>{key.toUpperCase()}</th>
+   })
+   }
+
+
+   getRowsData = function(){
+ var items = this.props.data;
+ var keys = this.getKeys();
+ return items.map((row, index)=>{
+ return <tr key={index}><RenderRow key={index} data={row} keys={keys}/></tr>
+ })
+ }
+
+ render() {
+ return (
+ <div>
+ <table>
+ <thead>
+ <tr>{this.getHeader()}</tr>
+ </thead>
+ <tbody>
+ {this.getRowsData()}
+ </tbody>
+ </table>
+ </div>
+
+ );
+ }
+}
+
+
+const RenderRow = (props) =>{
+    return props.keys.map((key, index)=>{
+    return <td key={props.data[key]}>{props.data[key]}</td>
+    })
+   }
+
 function submitForm(contentType, data, setResponse) {
  axios({
  url: `${API_BASE}/v1/file/read/`,
@@ -56,32 +110,7 @@ function App() {
  </form>
  <h1>response</h1>
 
- <table>
-     <thead>
-         <tr>
-             <th> sensors</th>
-             <th> mm_yyyy</th>
-             <th> total</th>
-             <th> count</th>
-             <th> per</th>
-             <th> dig </th>
-             <th> fs </th>
-             <th> sl </th>
-         </tr>
-     </thead>
-     <tbody >
-     {resp.map((x) => <tr >
-       <td>{x["sensors"]}</td>
-       <td>{x["mm_yyyy"]}</td>
-       <td>{x["total"]}</td>
-       <td>{x["count"]}</td>
-       <td>{x["per"]}</td>
-       <td>{x["dig"]}</td>
-       <td>{x["fs"]}</td>
-       <td>{x["sl"]}</td>
-       </tr>  )}
-     </tbody>
-   </table>
+<Table data={resp}/>
 
  </div>
  );
